5bb816f4f75e5eca97d26f6b80ac6526f794e5d4
kiralightyagami <crucisgamer98@gmail.com>
2025-05-07 16:00:54 +0530

    engineered prompt on top of bolt prompt

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 import { GoogleGenAI } from "@google/genai";
+import { getSystemPrompt } from "./prompts";
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
@@ -11,13 +12,17 @@ async function main() {
   const response = await ai.models.generateContentStream({
     model: "gemini-2.0-flash",
     config:{
-        maxOutputTokens: 500,
+        maxOutputTokens: 1024,
         temperature: 0.5,
+        systemInstruction: getSystemPrompt()
+
     },
     contents: [{
       role: "user",
-      parts: [{ text: 'write a short story about a cat' }] 
-  }]
+      parts: [{ text: 'write a short story about a cat' },
+              { text: 'write a short story about a dog'},
+              { text: 'write a short story about a bird'}] 
+      }]
   });
   
   for await (const chunk of response) {
