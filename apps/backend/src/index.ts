1c069e17266efad3d82fe4366193849fc964e972
kiralightyagami <crucisgamer98@gmail.com>
2025-05-08 13:45:24 +0530

    added seperate router for claude and gemini for switching between ai

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,92 +1,18 @@
 require("dotenv").config();
-import express from "express";
 import cors from "cors";
-import { GoogleGenAI } from "@google/genai";
-import { getSystemPrompt } from "./prompts";
-import {basePrompt as nodeBasePrompt } from "./defaults/node";
-import {basePrompt as reactBasePrompt } from "./defaults/react";
-import {BASE_PROMPT} from "./prompts";
-import { GenerateSchema, TemplateSchema } from "./types/types";
-import { authMiddleware } from "./middleware/middleware";
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-
-const ai = new GoogleGenAI({ apiKey:GEMINI_API_KEY });
+import express from "express";
+import { geminiRouter } from "./routes/geminiRouter";
+import { claudeRouter } from "./routes/claudeRouter";
+//import { authMiddleware } from "../middleware/middleware";
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 //app.use(authMiddleware);
 
-app.post("/template", async (req, res) => {
-    
-    const parsedData = TemplateSchema.safeParse(req.body)
-    if(!parsedData.success) {
-        res.status(400).json({message: "Invalid request"});
-        return;
-    }
-    const prompt = parsedData.data.prompt;
-
-    const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        config:{
-            maxOutputTokens: 200,
-            systemInstruction: " Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anthing extra"
-        },
-        contents: [{
-            role: "user",
-            parts: [{ text: prompt }]
-        }]
-    });
-
-    const answer = response.text?.trim().toLowerCase();
-    console.log('Answer:', answer);
-    console.log('Answer type:', typeof answer);
-    console.log('Answer length:', answer?.length);
-    
-    if(answer === "react") {
-        res.json({
-            prompts :[BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-            uiPrompts: [reactBasePrompt]
-        })
-        return;
-    }
-
-    if(answer === "node") {
-        res.json({
-            prompts :[BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
-            uiPrompts: [nodeBasePrompt]
-        })
-        return;
-    }
-   
-    res.status(403).json({message: "Invalid"});
-    return;
-        
-});
-    
-app.post("/generate", async (req, res) => {
-
-    const parsedData = GenerateSchema.safeParse(req.body)
-    if(!parsedData.success) {
-        res.status(400).json({message: "Invalid request"});
-        return;
-    }
-    const messages = parsedData.data.messages;
-    const response = await ai.models.generateContent({
-        model: "gemini-2.0-flash",
-        config:{
-            maxOutputTokens: 8000,
-            temperature: 0.5,
-            systemInstruction: getSystemPrompt()
-        },
-        contents: messages
-    });
-
-    console.log(response.text);
-
-    res.json({})
-
-});
-
+app.use("/api/v1/gemini", geminiRouter);
+app.use("/api/v1/claude", claudeRouter);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
